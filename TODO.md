a7e7b22ecc583ae71856dc3ac1fb09239b2b0e68
Robert K. Bell <robert.k.bell@gmail.com>
2016-07-03 17:21:53 +1000

    feat: serve placeholder files from a live-restarted dev server

--- TODO.md
+++ TODO.md
@@ -2,9 +2,12 @@
 # todo
 
 - [✓] get basics working
-  - [ ] use `wittr` scaffolding as a reference
+  - [✓] use `wittr` scaffolding as a reference
     - [✓] build process scaffolding
-    - [ ] server skeleton
-      - [ ] templating skeleton
-  - [ ] use cycle.js?
+    - [✓] server skeleton
+      - [✓] templating skeleton
 - [ ] get data from gtfs api / file (?)
+- [ ] use cycle.js?
+- [ ] use proper caching (`max-age`, invalidation, etc)
+- [ ] use data compression
+- [ ] use http/2
--- server/server.js
+++ server/server.js
@@ -1,10 +1,43 @@
 import koa from 'koa';
+import logger from 'koa-logger';
+import koaRouter from 'koa-router';
+import mount from 'koa-mount';
+import serve from 'koa-static';
+import send from 'koa-send';
+// noinspection JSFileReferences
+import indexTemplate from './templates/index'; // eslint-disable-line import/no-unresolved
+
+const router = koaRouter();
 
 const app = koa();
 
-app.use(function* helloBody() {
-  this.body = 'Hello World';
+app.use(logger());
+
+function serveBuilt(x) {
+  return koa().use(serve(`../public/${x}`));
+}
+
+app.use(mount('/js', serveBuilt('js')));
+app.use(mount('/css', serveBuilt('css')));
+app.use(mount('/imgs', serveBuilt('imgs')));
+
+router.get('/', function* root() {
+  this.body = indexTemplate();
 });
+
+function singleFile(file) {
+  return function* singleFileGenerator() {
+    yield send(this, file, { root: '../public' });
+  };
+}
+
+router.get('/manifest.json', singleFile('manifest.json'));
+router.get('/sw.js', singleFile('sw.js'));
+router.get('/sw.js.map', singleFile('sw.js.map'));
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 const start = options => app.listen(options.port);
 
