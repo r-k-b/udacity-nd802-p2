01aa13e2bf456e60d0b36cfd5031bee81082e0b3
Robert K. Bell <robert.k.bell@gmail.com>
2016-07-24 19:02:42 +1000

    feat: more messing with js

--- public/js/main/index.js
+++ public/js/main/index.js
@@ -1,5 +1,19 @@
 import IndexController from './IndexController';
 import trainSchedule from './trainSchedule';
+import { Observer } from 'rx';
+import log from 'loglevel';
+
+log.setDefaultLevel('trace');
+
+const tap = (name = '?') => Observer.create(
+  x =>
+    log.trace(`[${name}] next:`, x),
+  e =>
+    log.warn(`[${name}] error:`, e),
+  () =>
+    log.trace(`[${name}] completed.`)
+);
 
 IndexController(document.querySelector('.app-main')); // eslint-disable-line new-cap
-trainSchedule.main();
+
+trainSchedule.main.subscribe(tap('main'));
--- public/js/main/trainSchedule.js
+++ public/js/main/trainSchedule.js
@@ -1,11 +1,42 @@
-import { Task } from 'data.task';
-import controlAsync from 'control.async';
+import { Observable, Observer } from 'rx';
+import { DOM } from 'rx-dom';
+import log from 'loglevel';
+import { always, path, pathOr } from 'ramda';
 
-const Async = controlAsync(Task);
+log.setDefaultLevel('trace');
 
-const fetchStations = () => Async.fromPromise(fetch('/gtfs-data/stations.json'));
+const tap = (name = '?') => Observer.create(
+  x =>
+    log.trace(`[${name}] next:`, x),
+  e =>
+    log.warn(`[${name}] error:`, e),
+  () =>
+    log.trace(`[${name}] completed.`)
+);
 
-const main = fetchStations;
+const fetchStationList = Observable
+  .fromPromise(fetch('/gtfs-data/stations.json'))
+  .share();
+
+fetchStationList.subscribe(tap('fetch'));
+
+const fetchRoutes = ([station1ID, station2ID]) => Observable
+  .fromPromise(fetch(`/gtfs-data/stations.json?s1=${station1ID}&s2=${station2ID}`))
+  .share();
+
+const b1 = document.querySelector('[data-station-selector="departure"]');
+const b2 = document.querySelector('[data-station-selector="arrival"]');
+
+const getStationID = path(['target', 'value']);
+
+const deps = DOM.change(b1).map(getStationID);
+const arrs = DOM.change(b2).map(getStationID);
+
+const selectedStations = Observable.combineLatest(deps, arrs);
+
+const main = selectedStations
+  .selectMany(fetchRoutes);
+
 
 export default {
   main,
